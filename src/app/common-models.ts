e73f6818f0504788a9e35cb15c86928c3db1e99a
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-08-01 18:49:07 +0800

    index-component: dynamically load ui elements using *ngFor
    prepare the ui for binding with the backend

--- src/app/common-models.ts
+++ src/app/common-models.ts
@@ -1,7 +1,7 @@
 export class CommonModels {
 }
 export class Item {
-    constructor(public ListingId: string, public title: string, public sellerName: string, public sellerUID: string, public sellerImageURL: string, public Likes: number, public listedTime: Date, public price: number, public rating: number, public description: string, public transactionInformation: string, public procurementInformation: string, public category: string, public stock: number, public images: Array<string>, public isAdvert: boolean, public isUsed: boolean, public location: string) {
+    constructor(public listingId: string, public title: string, public sellerName: string, public sellerUID: string, public sellerImageURL: string, public Likes: number, public listedTime: Date, public price: number, public rating: number, public description: string, public transactionInformation: string, public procurementInformation: string, public category: string, public stock: number, public images: Array<string>, public isAdvert: boolean, public isUsed: boolean, public location: string) {
 
     }
 }
--- src/item-card/item-card.component.ts
+++ src/item-card/item-card.component.ts
@@ -7,11 +7,10 @@ import { Item } from 'src/app/common-models';
   styleUrls: ['./item-card.component.scss'],
 })
 export class ItemCardComponent implements OnInit {
-  item: Item
+  @Input() item: Item;
   constructor() { }
 
   ngOnInit() {
-    this.item = new Item("a", "testing", "testingseller", "0", "a", 10, new Date() , 23, 12, "lol", "test", "test", "clothes", 12, ["https://media.karousell.com/media/photos/products/2017/08/21/black_temasek_xxv_t_shirt_1503303024_271b1bcb.jpg"], false, false, "a");
   }
 
 }
