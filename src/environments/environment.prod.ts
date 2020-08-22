73ea5a123029a357c9c3725ea6bc8750b6e0afc1
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-08-23 02:27:44 +0800

    squash final bugs and prepare for submission

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBi0vKkdYtAclkL9-ZYYn-mBZmCu6GOGjU",
+    authDomain: "la-lumire.firebaseapp.com",
+    databaseURL: "https://la-lumire.firebaseio.com",
+    projectId: "la-lumire",
+    storageBucket: "la-lumire.appspot.com",
+    messagingSenderId: "8046173692",
+    appId: "1:8046173692:web:10d767282b9af6cd6c2679"
+  },
 };
